4821af18505a4f23ce8c4f26356d7f89562fa174
Kelsey Wu <kelseywu@college.harvard.edu>
2022-11-11 15:09:20 -0500

    load data

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ console.log("let's get started!")
 
 let data;
 
-d3.csv("data/bachelorette-data.csv", (csv) => {
-    console.log(csv)
+d3.csv("data/bachelorette-data.csv", (row) => {
+    row.date_size = +row.date_size
+    row.first_impression = +row.first_impression
+    row.season = +row.season
+    row.elim_week = +row.elim_week
+    row.winner = +row.winner
+    return row
+}).then((csv) => {
     data = csv
+    console.log(data)
 })
 
-console.log(data)
