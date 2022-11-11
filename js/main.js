7b383f1068a3b7cc5c5f8ff447b4e7a7206f1cd3
Kelsey Wu <kelseywu@college.harvard.edu>
2022-11-11 15:41:34 -0500

    update js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("let's get started!")
-
 let data;
 
 d3.csv("data/bachelorette-data.csv", (row) => {
@@ -12,5 +10,36 @@ d3.csv("data/bachelorette-data.csv", (row) => {
 }).then((csv) => {
     data = csv
     console.log(data)
+    drawDots(data)
 })
 
+function drawDots(data){
+    // Margin object with properties for the four directions
+    let margin = {top: 20, right: 40, bottom: 20, left: 20};
+
+    let padding = 30;
+
+    // Width and height as the inner dimensions of the chart area
+    let width = 600 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    let svg = d3.select("#dots")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+    var circles = svg.selectAll("dots")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => d.season * 10)
+        .attr("cy", (d,i) => i * 10)
+        .attr("r", 2)
+        .attr('fill', function(d){
+            if(d.winner === 1){
+                return 'blue'
+            }else{
+                return 'red'
+            }
+        })
+}
