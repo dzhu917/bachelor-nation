97bbd8858302f2a20964eef6b0e2052f71475034
dzhu917 <dianazhu@college.harvard.edu>
2022-11-13 21:12:00 -0500

    sorted colored dots

--- js/sankey.js
+++ js/sankey.js
@@ -11,6 +11,9 @@ class FirVis {
     initVis() {
         let vis = this
 
+        // sort data by FIR
+        vis.data.sort((a,b) => {return b.fir - a.fir})
+
         // Margin object with properties for the four directions
         vis.margin = {top: 20, right: 40, bottom: 20, left: 20};
 
@@ -20,8 +23,7 @@ class FirVis {
         vis.width = document.getElementById(vis.parentElement).getBoundingClientRect().width - vis.margin.left - vis.margin.right;
         vis.height = document.getElementById(vis.parentElement).getBoundingClientRect().height - vis.margin.top - vis.margin.bottom;
 
-        // create circles for each contestant (uniform, vertical, x = 0 + padding)
-        // color code by FIR value
+        // sort data by FIR
 
         vis.svg = d3.select("#" + vis.parentElement)
             .append("svg")
@@ -38,10 +40,18 @@ class FirVis {
                 return vis.padding + 10*(i % 10)
             })
             .attr('cy', function(d, i){
-                return vis.padding + 10*Math.floor(i / 10) // use modulo 3 for x
+                return vis.padding + 10*Math.floor(i / 10)
             })
             .attr('r', 3)
-            .attr('fill', 'black');
+            .attr('fill', function(d){
+                if(d.fir === 1){
+                    return 'rgba(255,49,49,0.62)'
+                } else{
+                    return '#0f4f65'
+                }
+            });
+
+        // create legend where red = FIR, blue = not FIR, grey '#525750' = eliminated
 
         vis.wrangleData();
     }
