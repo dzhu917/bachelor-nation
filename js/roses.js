367003772369a6e13aedec0fef31f0d54270110c
dzhu917 <dianazhu@college.harvard.edu>
2022-12-04 13:31:47 -0500

    add sparkles

--- js/roses.js
+++ js/roses.js
@@ -6,6 +6,8 @@ canvas.height = window.innerHeight;
 var context = canvas.getContext("2d");
 var img = new Image();
 img.src = "data/rose.gif"
+var img_sparkle = new Image();
+img_sparkle.src = "data/sparkles.png"
 
 var numRoses = 20;
 var rose = [];
@@ -14,6 +16,15 @@ for(var i = 0; i < numRoses; i++){
     var x = Math.random()*canvas.width;
     var y = Math.random()*canvas.height;
     rose[i] = new Rose(x, y);
+}
+
+var numHearts = 15;
+var heart = [];
+
+for(var i = 0; i < numHearts; i++){
+    var x = Math.random()*canvas.width;
+    var y = Math.random()*canvas.height;
+    heart[i] = new Heart(x, y);
 }
 
 function Rose(x, y){
@@ -31,12 +42,31 @@ function Rose(x, y){
     }
 }
 
+function Heart(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.fall = function(){
+        this.y = this.y+1;
+        if(this.y > canvas.height){
+            this.y = 0;
+        }
+    }
+    this.show = function(){
+        context.drawImage(img_sparkle, this.x, this.y, 20, 20);
+    }
+}
+
 function draw() {
     context.fillStyle = '#242635';
     context.fillRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < numRoses; i++) {
         rose[i].show();
         rose[i].fall();
+    }
+    for (var i = 0; i < numHearts; i++) {
+        heart[i].show();
+        heart[i].fall();
     }
 }
 
